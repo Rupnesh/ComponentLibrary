81617012cb229480ce35f40c507d2bfc75099312
Rupnesh <rupnesh.s@commerceiq.ai>
2022-05-27 14:03:04 +0530

    fix: comment removed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -42,7 +42,6 @@ export default {
         <!-- <Button class="btn" type="hollow" label="Hollow Button" size="s" :click-fn="() => clickFn('')" />
         <Button class="btn" type="rounded" label="Rounded Button" size="m" :click-fn="() => clickFn('')" />
         <Button class="btn" type="filled" label="Filled Button" size="l" :click-fn="() => clickFn('')" /> -->
-          <p>Heloo</p>
       </div>
     </div>
   </div>
